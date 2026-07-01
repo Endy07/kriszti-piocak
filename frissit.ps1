ad00cb6294a3d8e0a03793cb2261873231ce5913
Endy07 <endy_311@hotmail.com>
2026-07-01 14:08:32 +0200

    Automatikus feltöltés: alkönyvtárak beolvasása

--- frissit.ps1
+++ frissit.ps1
@@ -1,4 +1,7 @@
-$files = Get-ChildItem -Filter *.html | Where-Object Name -ne 'index.html' | Select-Object -ExpandProperty Name
+$basePath = (Get-Location).Path
+$files = Get-ChildItem -Filter *.html -Recurse | Where-Object Name -ne 'index.html' | ForEach-Object {
+    $_.FullName.Substring($basePath.Length + 1).Replace('\', '/')
+}
 if ($files) {
     $files | ConvertTo-Json | Set-Content files.json -Encoding UTF8
 } else {
